7bfd10cc6c014435120c12227192bca9692127ef
tavac <caio.ta@outlook.com>
2021-12-10 11:43:33 -0500

    accordian buttons are working now. forgot to add the bootstrap scripts to index.html

--- react-lessonform/src/components/Question.js
+++ react-lessonform/src/components/Question.js
@@ -4,7 +4,7 @@ class Question extends Component {
   render() {
     return (
       <button className="accordion-button collapsed"
-        data-bs-toggle="collapse" type="button" id={`${this.props.id}`}>
+        data-bs-toggle="collapse" type="button" data-bs-target="#Answers">
         <div className="input-group mb-3">
           <span className="input-group-text">Question:</span>
           <input className="form-control" type="text" aria-label="Question 1 input" aria-describedby="Q1-input"></input>
